5794734fbe2259a1cef7b73fcc5b491e88285332
Paul Enestvedt <enestvedt@gmail.com>
2021-09-23 10:14:20 -0500

    added preloaders to data tables

--- ML-app/static/js/forecast.js
+++ ML-app/static/js/forecast.js
@@ -1,19 +1,38 @@
-// Fetch the JSON data and console log it
-mockData = []
+/*********************************************************************************
+load the page
+**********************************************************************************/
+
+// hide output preloader until button click
+var outputLoader = document.querySelector('#outputLoader');
+outputLoader.style.display = "none";
+
+// the page displays today's date - js build m/d/y format
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+
+// preloader - started on page load - removed when promise hitting "/import" route kept
+var inputLoader = document.querySelector('#inputLoader');
+inputLoader.classList.add('spin');
+
+// the Flask app "/import" route reads from the api and sends the data to this page
+// we display these in an html table in buildTable function
+// remove the preloader and load the data
 
 d3.json("/import").then(function(myData) {
-    // load all data
+    inputLoader.style.display = "none";
     buildTable(myData);
-    mockData = myData
 });
 
-// Use D3 to select the table body
+// select the table body - table will be populated with data from api - Flask route "/import"
 var forecast_body = d3.select("#forecast_body");
 
-//Function to populate table with all or user filtered data
-// Build table by loop myData and get keys/vals write cells
+// Function to populate table
+// Build table by looping myData (the api data) and get keys/vals write cells
 function buildTable(forecastData){
-    forecast_body.html("");//remove any rows of data from the table
+    forecast_body.html("");//remove any rows of data from the table - start fresh each time
     forecastData.forEach((date) => {
         // Append one table row per object
         var row = forecast_body.append("tr");
@@ -26,25 +45,37 @@ function buildTable(forecastData){
     });
 };
 
-var button = d3.select("#predict-btn");
+/*********************************************************************************
+call prediction model on button click 
+**********************************************************************************/
 
-// //handler
+// select the button - users click to predict solar panel array output
+var button = d3.select("#predict-btn");
+//handler calls "apiCall" function
 button.on("click", apiCall);
 
+// make api call to Flask rout "/predict2" - the ML model lives here and returns an output prediction
+// start preloader on click and remove when promise kept
+// call outputTable function passing api result
 function apiCall() {
     
+    // preloader - started on button click - removed when promise hitting "/predict2" route kept
+    outputLoader.style.display = "block";
+    outputLoader.classList.add('spin');
+
     d3.json("/predict2").then(function(result) {
         console.log(result);
+        console.log(result.result);
+        outputLoader.style.display = "none";
         outputTable(result.result);
     });
     
 };
 
-// Use D3 to select the table body
+// select the table body - build output prediction table here
 var output_body = d3.select("#output_body");
 
-//Function to populate table with all or user filtered data
-// Build table by loop myData and get keys/vals write cells
+// Build table by looping result and getting keys/vals write cells
 function outputTable(prediction){
     output_body.html("");//remove any rows of data from the table
     console.log(prediction);
@@ -61,5 +92,3 @@ function outputTable(prediction){
         });
     });
 };
-
-
